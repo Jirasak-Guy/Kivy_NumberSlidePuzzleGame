fe78cd6b78fa9f1f65fa1a5e0d9b681927d22b56
จิรศักดิ์ ทรงศรี <jirasak.s.guy@gmail.com>
2023-02-27 22:19:50 +0700

    สร้างฟังก์ชันเกี่ยวกับการสร้างปุ่มตัวเลข

--- number_puzzle_game.py
+++ number_puzzle_game.py
@@ -1,8 +1,27 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+
 
 class SlidingPuzzle(GridLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 4
+        self.rows = 4
+        self.tiles = []
+        self.generate_tiles()
+        self.add_tiles()
+
+    def generate_tiles(self):
+        nums = list(range(1, 16))
+        nums.append(None)
+        for num in nums:
+            button = Button(text=str(num) if num else "", font_size=50)
+            self.tiles.append(button)
+
+    def add_tiles(self):
+        for tile in self.tiles:
+            self.add_widget(tile)
 
 
 class MyGame(App):
